7c77e6a85913e7424404626a8f41bbaa83f7cbde
fuadmoin <fuadamoin@gmail.com>
2023-04-11 18:09:57 +0300

    Make required changes.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    book: booksReducer,
+    category: categoriesReducer,
+  },
 });
 
 export default store;
