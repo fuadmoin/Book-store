1995fece41a07794f27da70afeccb43d5cba167b
fuadmoin <fuadamoin@gmail.com>
2023-04-13 16:21:50 +0300

    Make required changes
    I created addBooks and removeBook components and used them to add and remove the book from the array.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,26 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addBook, removeBook } from '../redux/books/booksSlice';
+import AddBooks from './addBooks';
+import RemoveBooks from './removeBook';
 
 const Home = () => {
   const books = useSelector((state) => state.book.books);
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+
   const dispatch = useDispatch();
-  const handleChange = (e) => {
-    if (e.target.name === 'title') {
-      setTitle(e.target.value);
-    } else if (e.target.name === 'author') {
-      setAuthor(e.target.value);
-    }
+  const add = (book) => {
+    dispatch(addBook(book));
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (title.trim() && author.trim()) {
-      dispatch(addBook({ title, author, id: uuidv4() }));
-      setTitle('');
-      setAuthor('');
-    }
+  const remove = (id) => {
+    dispatch(removeBook(id));
   };
+
   return (
 
     <div>
 
-      { books.map((book) => (
-        <div key={book.id}>
-          <span>
-            author:
-            {book.author}
-          </span>
-          {'  '}
-          <span>
-            title:
-            {book.title}
-          </span>
-          {'  '}
-          <button type="button" onClick={() => dispatch(removeBook(book.id))}>Remove</button>
-        </div>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input name="author" type="text" placeholder="author" value={author} onChange={handleChange} />
-        <input name="title" type="text" placeholder="title" value={title} onChange={handleChange} />
-        <button type="submit">Add</button>
-      </form>
+      <RemoveBooks books={books} removeBook={remove} />
+      <AddBooks addBooks={add} />
     </div>
   );
 };
