52d45ce3240702d2bda1c4b02cd5c18ebb6a2223
fuadmoin <fuadamoin@gmail.com>
2023-04-14 13:19:52 +0300

    Hit the api.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeBook, fetchBooks } from '../redux/books/booksSlice';
 import Buttons from './Buttons';
 
-const BookList = ({ books }) => {
+const BookList = () => {
+  const books = useSelector((state) => state.book.books);
+  const book = useSelector((state) => state.book);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   const remove = (id) => {
     dispatch(removeBook(id));
   };
+
   return (
 
     <div>
-      { books.map((book) => (
-        <div key={book.id}>
-          <span>
-            author:
-            {book.author}
-          </span>
-          {'  '}
-          <span>
-            title:
-            {book.title}
-          </span>
-          {'  '}
-          <Buttons removeBook={remove} id={book.id} />
-        </div>
-      ))}
+      {!book.loading && books.length ? (
+        books.map((book) => (
+          <div key={book.item_id}>
+            <span>
+              author:
+              {book.author}
+            </span>
+            {'  '}
+            <span>
+              title:
+              {book.title}
+            </span>
+            {'  '}
+            <Buttons removeBook={remove} id={book.item_id} />
+          </div>
+
+        ))) : null}
+
     </div>
   );
 };
-BookList.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-  })).isRequired,
-};
+
 export default BookList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { useDispatch } from 'react-redux';
+import { addBooks } from '../redux/books/booksSlice';
 import AddBooks from './addBooks';
 import BookList from './BookList';
 
 const Home = () => {
-  const books = useSelector((state) => state.book.books);
-
   const dispatch = useDispatch();
   const add = (book) => {
-    dispatch(addBook(book));
+    dispatch(addBooks(book));
   };
 
   return (
 
     <div>
 
-      <BookList books={books} />
+      <BookList />
       <AddBooks addBooks={add} />
     </div>
   );
